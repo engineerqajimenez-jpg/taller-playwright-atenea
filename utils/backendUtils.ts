eaf0d278f8efcc4dc626e098f456500170b3115b
José Jiménez <engineerqajimenez@gmail.com>
2026-01-30 01:42:17 -0400

    Se agregan escenarios de Bakcend

---
+++ utils/backendUtils.ts
@@ -0,0 +1,23 @@
+import {Page, Locator, } from '@playwright/test';
+
+export class backendUtils{
+  readonly page: Page;
+  
+  
+  constructor(page: Page) {
+       this.page = page;
+  }
+
+  async enviarRequestDeBackend(endpoint: string, data: any) {
+    const response = await this.page.request.post(endpoint, {
+      headers: {
+        'Accept': 'application/VND.github.v3+json',
+        'Content-Type': 'application/json'
+      },
+      data: data
+    });
+    const responseBody = await response.json();
+    return responseBody;
+}
+
+}
