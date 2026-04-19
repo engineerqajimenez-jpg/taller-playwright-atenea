8d4bdff73118039e02188d6496918d11103e3b24
José Jiménez <engineerqajimenez@gmail.com>
2026-04-19 19:40:59 -0400

    Se arregla clonado #17

--- tests/auth.setup.ts
+++ tests/auth.setup.ts
@@ -1,7 +1,7 @@
 import { test as setup } from '@playwright/test';
 
 setup('login usuario envia', async ({ page }) => {
-  await page.goto('/login'); 
+  await page.goto('http://localhost:3000/login'); 
   await page.fill('#email', process.env.USER_ENVIA_EMAIL!); // Usa secretos de GitHub
   await page.fill('#password', process.env.USER_ENVIA_PASS!);
   await page.click('button[type="submit"]');
